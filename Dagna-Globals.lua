346ad87f5210a1e026ba1ef4c6cccfd6905e64c8
DrakeFS <drake.fsx@gmail.com>
2019-11-10 16:15:41 -0500

    Update and rename Dagna.lua to Dagna-Globals.lua

--- Dagna-Globals.lua
+++ Dagna-Globals.lua
@@ -1,3 +1,8 @@
+-- Gear Definitions
+gear.CPCape = {name="Mecisto. Mantle", augments={'Cap. Point+44%','INT+1','"Mag.Atk.Bns."+1','DEF+2',}}
+gear.HercMB = { name="Herculean Gloves", augments={'VIT+15','"Mag.Atk.Bns."+24','Accuracy+6 Attack+6','Mag. Acc.+17 "Mag.Atk.Bns."+17',}}
+
+-- Override sets
 sets.reive = {neck="Ygnas's Resolve +1"}
 
 -- Global intercept on midcast.
